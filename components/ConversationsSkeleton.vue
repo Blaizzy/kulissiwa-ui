0f01d40a90c33fe11a61503a13ee3c98dcde6a05
Prince Canuma <prince.gdt@gmail.com>
2023-10-20 17:45:13 +0200

    hybrid fuzzy search and pagination

--- components/ConversationsSkeleton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="p-3 rounded-full animate-pulse">
-        <div class="flex items-center justify-between">
-            <div class="truncate text-gray-300 bg-gray-300 h-4 w-full rounded"></div>
-        </div>
-    </div>
-</template>
-
